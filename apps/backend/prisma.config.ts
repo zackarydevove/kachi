b507b2c98eb3169585383ce14a1b9e98d3db5a8b
zackarydevove <zackary.devove@hotmail.com>
2026-01-21 17:22:45 +0700

    add dummy db url for app runner build

--- apps/backend/prisma.config.ts
+++ apps/backend/prisma.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    // Dummy to bypass error during build as app runner doesn't have env yet
+    url:
+      env('DATABASE_URL') || 'postgresql://dummy:password@localhost:5432/dummy',
   },
 });
