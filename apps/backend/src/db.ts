2b18696edbc220abe868cc0f599017d6df1e9b18
zackarydevove <zackary.devove@hotmail.com>
2026-01-21 16:59:23 +0700

    fix prisma 7.2

--- apps/backend/src/db.ts
+++ apps/backend/src/db.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from '@generated/prisma';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
